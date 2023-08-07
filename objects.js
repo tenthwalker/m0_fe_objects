5e39211743e3e49e46030198f1270761868e96ec
Arden Ranta <arranta20@gmail.com>
2023-08-07 15:32:15 -0700

    Complete second part of assignment

--- objects.js
+++ objects.js
@@ -13,43 +13,49 @@ var foods = {
 
 // Write code that prints all of the 'keys' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(foods));
 
 // Write code that prints all of the 'values' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.values(foods));
 
 // Write code that prints the value of the second food of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(foods.grapes)
 
 // Write code that adds a food to the foods object. 
 // Then, print the updated object:
-// YOUR CODE HERE
+foods.carrots = 14;
+console.log(Object(foods));
 
 
 // -------------------
 // Part 2: Email
 // -------------------
 
-
 // Think about all the pieces of information associated with one single email in your inbox.
 // It has a sender, a subject, ...
 
 // Declare a variable that stores an object. Each key should be an attribute of an email and each
 // value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+var email = {
+  sender: "info@tech.net",
+  recipient: "help@tech.net",
+  subject: "New user privileges",
+  attachment: true,
+  content: "Please give all users copier access."
+};
 
 // Write code that logs your email object to the terminal.
-
+console.log(Object(email));
 
 // Write code that logs all of the 'keys' of the email object 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(email));
 
 // Write code that logs all of the 'values' of the email object 
 // you created above:
-// YOUR CODE HERE
-
+console.log(Object.values(email));
 
 // -------------------
 // Part 3: Many Emails - CHALLENGE!
@@ -60,42 +66,63 @@ var foods = {
 
 // Check out the following example of an array of Instagram posts:
 
-
-var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
-
+// var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // An Array of Objects is probably more realistic. Objects are a data type that's great
 // for storing more complex data. Below, we can store multiple key-value pairs
 // that relate to each individual Instagram post:
 
+// posts = [
+//   {
+//     imageSrc: "./images/beach.png",
+//     caption: "At the beach with my besties",
+//     timestamp: "4:37 PM August 13, 2019",
+//     number_likes: 0,
+//     comments: []
+//   }, 
+//   {
+//     imageSrc: "./images/holiday-party.png",
+//     caption: "What a great holiday party omg",
+//     timestamp: "11:37 PM December 31, 2019",
+//     number_likes: 13,
+//     comments: []
+//   }
+// ];
 
-posts = [
-  {
-    imageSrc: "./images/beach.png",
-    caption: "At the beach with my besties",
-    timestamp: "4:37 PM August 13, 2019",
-    number_likes: 0,
-    comments: []
-  }, 
-  {
-    imageSrc: "./images/holiday-party.png",
-    caption: "What a great holiday party omg",
-    timestamp: "11:37 PM December 31, 2019",
-    number_likes: 13,
-    comments: []
-  }
-];
+// console.log(posts);
+// console.log(posts[0]);
 
-console.log(posts);
-console.log(posts[0]);
-
-
-// The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-// Objects. Each of those Objects has 4 key-value pairs. This may LOOK 
+// The code snippet above shows an Array with 2 elements. Each element in the Array is an 
+// Object. Each of those Objects has 4 key-value pairs. This may LOOK 
 // a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 // good to have some exposure before going into Mod 1.
-
 
 // YOU DO: Create an array of at least 3 EMAIL Objects, using the same 
 // key-value pairs you used in your email Object above.
 // Then, print the email Array to the Terminal.
+
+var email2 = [
+  {
+    sender: "info@tech.net",
+    recipient: "help@tech.net",
+    subject: "New user privileges",
+    attachment: true,
+    content: "Please give all users copier access."
+  },
+  {
+    sender: "billing@boxstore.com",
+    recipient: "ap@warehouse.net",
+    subject: "Overdue invoice",
+    attachment: true,
+    content: "Please pay us, your attached invoice is past due."
+  },
+  {
+    sender: "silly@goober.com",
+    recipient: "bigboy@comedy.org",
+    subject: "got a goof for ya",
+    attachment: false,
+    content: "joke's on you, I don't have anything funny to say"
+  }
+];
+
+console.log(Object(email2))
